4c36f6b6e3f0ecefe521c9b4a47e35da582ce947
Micah Elliott <mde@MicahElliott.com>
2011-07-31 17:59:21 -0700

    Total reorg to support running from anywhere.
    Create your own font anywhere and use any dir for fontpath.
    
    Updated documentation too.

--- xfont-install.zsh
+++ xfont-install.zsh
@@ -4,32 +4,49 @@
 #
 # See README for more details.
 
-usage="
-usage: ${0:t} FONT-NAME.bdf
+usage="Usage: ${0:t} FONT-NAME.bdf
 
 Generate and install a PCF.
 
 To start a new font, copy an existing BDF:
-  cp orp-medium.bdf my-font.bdf
+  cp .../orp-medium.bdf lib/my-font.bdf
 
 Hack your new font:
-  gbdfed my-font.bdf
+  gbdfed lib/my-font.bdf
 
 Install it:
-  ./xfont-install.zsh my-font
+  $0:t lib/my-font.bdf
 "
 
-font=${1?Must provide font name sans ext}
-font=${font:t:r}
-trunc="$font-trunc"
-#resources="${0:h}/resources"
-resources="${0:h}"
+# Check for a bunch of misuses.
+[[ $# > 0 ]] || { print $usage; exit 1 }
+bdf=${1?Must provide font.bdf file}
+[[ -f $bdf ]]           || { print "ERROR: missing $bdf file.\n\n$usage"; exit 1 }
+[[ $bdf:t:e == 'bdf' ]] || { print "ERROR: only will work with .bdf files.\n\n$usage"; exit 1 }
+bdfdir=$bdf:h
+font=$bdf:t:r
+cd $0:h
+basedir=$PWD
+cd -
+libdir=$basedir/lib
+[[ -d $libdir ]] || { print $usage; exit 1 }
+# Do the generation work from the local misc dir.
+pcfdir=$PWD/misc
+[[ -d $pcfdir ]] || { print "INFO: creating a misc dir to sit on your font path"; mkdir $pcfdir }
 
+# Mysterious temp file.
+trunc="$font-trunc"
+# Needed for something UTF stuff.
+utfs=$libdir/8859-1.TXT
+
+# Check for system utility dependencies.
 deps=( bdftruncate.pl ucs2any bdftopcf mkfontdir gzip xset )
 print "Checking for dependencies…"
 for d in $deps; do
     which $d >/dev/null || exit 1
 done
+
+pushd $bdfdir
 
 # Generate intermediate truncated file.
 # Performance workaround for sparse fonts.
@@ -38,7 +55,7 @@ done
 bdftruncate.pl 'U+3200' <$font.bdf >$trunc.bdf
 
 # Generate BDF fonts containing subsets of ISO 10646-1 codepoints.
-ucs2any +d $font.bdf $resources/8859-1.TXT  ISO8859-1
+ucs2any +d $font.bdf $utfs ISO8859-1
 
 # Generate the PCF.
 bdftopcf $trunc.bdf >$font.pcf
@@ -52,8 +69,20 @@ gzip -f -9 $font.pcf
 # Clean up junk.
 rm $trunc.bdf
 
+pushd $pcfdir
+mv ~1/$font.pcf.gz .
+
 # Add this new PCF-GZ to $PWD/fonts.dir
 mkfontdir
 
 # Tell X that this font is now available.
+# Stupid stupid xsel bug makes adding multiple times cause duplicate
+# path entries, so have to delete the dir (which successfully removes
+# all duplicates), and then add. Use `xset q` to see everything.
+xset -fp $PWD
 xset +fp $PWD
+
+# Show new font path.
+xset q |grep -A1 'Font Path:'
+print "\nYou can remove this path if you messed up with:"
+print "  xset -fp $PWD"
--- xfont-test.zsh
+++ xfont-test.zsh
@@ -5,6 +5,6 @@
 #
 # Also a placeholder for more extensive future tests.
 
-cat ${0:h}/UTF-8-demo.txt
+cat ${0:h}/lib/UTF-8-demo.txt
 
 for c in {1..7}; do echo -e "\e[0;$c;3${c}muNitErest1n6 5tR|ng"; done
